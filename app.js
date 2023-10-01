7d56d8069e1dbe91303cbbecb1651424a97fd9f2
MajaNik86 <majanik86@gmail.com>
2023-10-01 12:06:11 +0200

    added the logic behind posting a destination and writing it in a file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const path = require('path');
 
@@ -6,6 +7,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
     const htmlFilePath = path.join(__dirname, 'views', 'index.html');
@@ -21,9 +23,28 @@ app.get('/recommend', function (req, res) {
     const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
     res.sendFile(htmlFilePath);
 })
+
+app.post('/recommend', function (req, res) {
+    const destination = req.body;
+    const filePath = path.join(__dirname, 'data', 'destinations.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const storedDestinations = JSON.parse(fileData);
+
+    storedDestinations.push(destination);
+    fs.writeFileSync(filePath, JSON.stringify(storedDestinations))
+    res.redirect('/confirm')
+})
+app.get('/confirm', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+    res.sendFile(htmlFilePath);
+})
 app.get('/about', function (req, res) {
     const htmlFilePath = path.join(__dirname, 'views', 'about.html');
     res.sendFile(htmlFilePath);
 })
+
+
+
 
 app.listen(3000);
