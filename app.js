65d7137d4ae6598ec3998641f9f37236e0e026c5
MajaNik86 <majanik86@gmail.com>
2023-10-01 12:27:53 +0200

    added ejs for displaying templates, render entered destinations and also if there is no destinations

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 
-const path = require('path');
-
+const path = require("path");
 
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }))
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get('/', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
-    res.sendFile(htmlFilePath);
-})
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/destinations', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'destinations.html');
-    res.sendFile(htmlFilePath);
-})
+app.get("/", function (req, res) {
+    res.render("index");
+});
 
-app.get('/recommend', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
-    res.sendFile(htmlFilePath);
-})
+app.get("/destinations", function (req, res) {
+    const filePath = path.join(__dirname, "data", "destinations.json");
 
-app.post('/recommend', function (req, res) {
+    const fileData = fs.readFileSync(filePath);
+    const storedDestinations = JSON.parse(fileData);
+
+    res.render("destinations", {
+        numOfDestinations: storedDestinations.length,
+        destinations: storedDestinations,
+    });
+});
+
+app.get("/recommend", function (req, res) {
+    const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+    res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
     const destination = req.body;
-    const filePath = path.join(__dirname, 'data', 'destinations.json');
+    const filePath = path.join(__dirname, "data", "destinations.json");
 
     const fileData = fs.readFileSync(filePath);
     const storedDestinations = JSON.parse(fileData);
 
     storedDestinations.push(destination);
-    fs.writeFileSync(filePath, JSON.stringify(storedDestinations))
-    res.redirect('/confirm')
-})
-app.get('/confirm', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
-    res.sendFile(htmlFilePath);
-})
-app.get('/about', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
-    res.sendFile(htmlFilePath);
-})
-
-
-
+    fs.writeFileSync(filePath, JSON.stringify(storedDestinations));
+    res.redirect("/confirm");
+});
+app.get("/confirm", function (req, res) {
+    res.render("confirm");
+});
+app.get("/about", function (req, res) {
+    res.render("about");
+});
 
 app.listen(3000);
